5153b6ab031bc4ee9710f1a67d045c78adfeaaa9
SANA <SANAL@colas.com>
2025-02-18 02:57:19 +0100

    second commit

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 // Servir les fichiers statiques depuis le dossier public
-app.use(express.static(path.join(__dirname, 'vercel_1')));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Rediriger toutes les routes vers index.html pour les SPA
-app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname, 'vercel_1', 'index.html'));
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
 app.listen(PORT, () => {
